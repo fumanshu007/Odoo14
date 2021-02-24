b9229b01b79ba35c74e383907c85d08949351c84
Randy <64906668+fumanshu007@users.noreply.github.com>
2021-02-24 13:19:57 +0100

    Add files via upload

---
+++ account_partner_default_account/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2017-2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{'name': 'Partner Default Account',
+ 'author': 'Onestein',
+ 'website': 'https://www.onestein.eu',
+ 'version': '14.0.1.0.1',
+ 'license': 'AGPL-3',
+ 'category': 'Finance',
+ 'depends': [
+     'account',
+ ],
+ 'data': [
+     'views/res_partner.xml',
+ ],
+ 'installable': True,
+ }
