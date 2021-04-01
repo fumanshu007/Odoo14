fd1120eef992e9f4c8aac2ede884d29980495c68
Randy <64906668+fumanshu007@users.noreply.github.com>
2021-04-01 16:12:50 +0200

    Add files via upload

---
+++ website_sendcloud/models/__init__.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import abstract_sendcloud_onboarding
+from . import delivery_carrier
+from . import sale_order
+from . import sendcloud_parcel
+from . import website
